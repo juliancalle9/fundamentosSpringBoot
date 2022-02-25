62f9e21625b3533696d97894797ff9d52a182abf
Julian Calle Velasquez <juliancalle9@mgail.com>
2022-02-24 19:08:52 -0500

    usando pojo

--- src/main/java/com/fundamentos/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/fundamentos/FundamentosApplication.java
@@ -1,7 +1,10 @@
 package com.fundamentos.fundamentos;
 
 import com.fundamentos.fundamentos.bean.MyBean;
+import com.fundamentos.fundamentos.bean.MyBeanWithProperties;
+import com.fundamentos.fundamentos.bean.MyBeanWithPropertiesImplement;
 import com.fundamentos.fundamentos.component.ComponentDependency;
+import com.fundamentos.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 
 	private MyBean myBean;
+	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	//Se inyectara la dependencia, se inyecta la interface
 	private ComponentDependency componentDependency;
@@ -18,9 +23,11 @@ public class FundamentosApplication implements CommandLineRunner {
 	Cuando se quieren implementar dos clases de una dependencia hay que indicarle cual dependencia usar por medio de
 	Qualifier
 	* */
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
+		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -31,5 +38,8 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + "" + userPojo.getAge());
+
 	}
 }
